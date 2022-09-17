c2ec5794dc88bb90c0773bd32c5def7b0490f35c
VolodymyrIpatenko <81745231+VolodymyrIpatenko@users.noreply.github.com>
2022-09-17 22:48:52 +0300

    с

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// const { check } = require('prettier');
+
 const form = document.querySelector('form');
 const username = document.getElementById('username');
 const email = document.getElementById('email');
@@ -5,12 +7,8 @@ const messageField = document.querySelector('textarea');
 const displayName = document.querySelector('.user-name');
 const displayEmail = document.querySelector('.user-email');
 const displayMessage = document.querySelector('.form-control__error_message');
-
-const setError = (element, message) => {
-  const inputControl = element.parentElement;
-  const errorDisplay = inputControl.querySelector('.form-control__error');
-  errorDisplay.innerText = message;
-};
+const checkmark = document.querySelector('.checkmark');
+const checkmarkEmail = document.querySelector('.checkmark_email');
 
 const isValidEmail = (email) => {
   const re =
@@ -24,21 +22,23 @@ const validateInputs = () => {
   const messageFieldValue = messageField.value.trim();
 
   if (usernameValue === '') {
-    setError(username, 'Username is required');
+    displayName.innerText = 'Name is required';
   } else if (usernameValue.length > 0) {
     displayName.innerText = '';
     username.style.borderColor = 'green';
+    checkmark.style.display = 'block';
   }
   if (emailValue === '') {
-    setError(email, 'Email is required');
+    displayEmail.innerText = 'Write your email';
   } else if (!isValidEmail(emailValue)) {
-    setError(email, 'Provide a valid email address');
+    displayEmail.innerText = 'Provide a valid email address';
   } else {
     email.style.borderColor = 'green';
     displayEmail.innerText = '';
+    checkmarkEmail.style.display = 'block';
   }
   if (messageFieldValue === '') {
-    setError(messageField, 'Your message is important for us');
+    displayMessage.innerText = 'Your message is important for us';
   } else if (messageFieldValue.length > 1) {
     messageField.style.borderColor = 'green';
     displayMessage.innerText = '';
